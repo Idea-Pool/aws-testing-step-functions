e175cc457db62e03b04920d65d4135d81df4b850
Kornel Filep <kornel.filep@gmail.com>
2023-06-27 14:16:48 +0200

    pass the out bucket name to the lambda via env variable in cf template

--- grayscaler/src/index.ts
+++ grayscaler/src/index.ts
@@ -1,31 +1,9 @@
-import {CloudFormationClient, DescribeStackResourceCommand} from '@aws-sdk/client-cloudformation';
 import {GetObjectCommand, PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
 import {Handler} from 'aws-lambda';
 import * as Jimp from 'jimp';
 
-const getBucketName = async (stackName: string, logicalResourceId: string) => {
-    const cloudformationClient = new CloudFormationClient({});
-    const describeStackResourceParams = {
-        StackName: stackName,
-        LogicalResourceId: logicalResourceId
-    };
-
-    try {
-        const describeStackResourceCommand = new DescribeStackResourceCommand(describeStackResourceParams);
-        const response = await cloudformationClient.send(describeStackResourceCommand);
-        const bucketName = response.StackResourceDetail!.PhysicalResourceId;
-        console.log("S3 Bucket Name:", bucketName);
-        return bucketName;
-    } catch (error) {
-        console.error("Error retrieving bucket name:", error);
-    }
-};
-
-
 export const handler: Handler = async function (event, context) {
     const s3Client = new S3Client({});
-    const stackName = "image-processing-demo"
-    const outBucketName = await getBucketName(stackName, "ImageOutBucket");
 
     const command = new GetObjectCommand({Bucket: event.bucket, Key: event.file})
     const s3Item = await s3Client.send(command);
@@ -36,7 +14,7 @@ export const handler: Handler = async function (event, context) {
 
     const imageBuffer = await image.getBufferAsync(Jimp.MIME_PNG);
     const uploadCommand = new PutObjectCommand({
-        Bucket: outBucketName,
+        Bucket: process.env.OUT_BUCKET,
         Key: `${event.file}-grayscale.png`,
         Body: imageBuffer
     })
